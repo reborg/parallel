96903a9de6e24767ddf332009c3df2b06a0268a5
reborg <reborg@reborg.net>
2017-10-24 10:51:32 +0100

    .

---
+++ README.md
@@ -0,0 +1,10 @@
+# xduce
+
+Transducers, helpers and transducer-related utils.
+
+## License
+
+Copyright © 2017 reborg.net
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject xduce "0.1.0"
+  :description "Transducers and beyond."
+  :url "https://github.com/reborg/xduce"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :source-paths ["src" "playground"]
+  :plugins [[lein-nodisassemble "0.1.3"]])
