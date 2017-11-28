344563239428b30e007924fcada28d96637d0b81
reborg <reborg@reborg.net>
2017-11-28 22:28:52 +0000

    No need to reify callable.

--- src/parallel/foldmap.clj
+++ src/parallel/foldmap.clj
@@ -3,17 +3,21 @@
   (:import [clojure.lang
             Get
             PersistentHashMap
+            PersistentHashMap$INode
             PersistentHashMap$ArrayNode
             PersistentHashMap$BitmapIndexedNode
             PersistentHashMap$HashCollisionNode]
            [java.util.concurrent Callable]
            [java.util ArrayList]))
 
-(set! *warn-on-reflection* true)
+(set! *warn-on-reflection* false)
 
-(defn- agetter [node] (Get/array node))
-
-(defn- callable [f] (reify Callable (call [this] (f))))
+(defn- agetter
+  "Trickiness, this needs to be indirected ATM.
+  Prevent Clojure from inlining the static call
+  into a class that doesn't have protected access
+  to clojure.lang. Will throw reflection warning."
+  [node] (Get/array node))
 
 (defn- fold-tasks [^ArrayList tasks combinef]
   (cond
@@ -21,7 +25,7 @@
     (== 1 (.size tasks)) (.call ^Callable (.get tasks 0))
     :else (let [t1 (.subList tasks 0 (quot (.size tasks) 2))
                 t2 (.subList tasks (quot (.size tasks) 2) (.size tasks))
-                forked (#'r/fjfork (#'r/fjtask (callable #(fold-tasks t2 combinef))))]
+                forked (#'r/fjfork (#'r/fjtask #(fold-tasks t2 combinef)))]
             (combinef (fold-tasks t1 combinef)
                       (#'r/fjjoin forked)))))
 
@@ -33,14 +37,11 @@
   PersistentHashMap
   (fold [m n combinef reducef]
     (#'r/fjinvoke
-      (reify
-        Callable
-        (call [this]
-          (let [ret (combinef)
-                ret (if (Get/root m) (combinef ret (fold (Get/root m) n combinef reducef)) ret)]
-            (if (Get/hasNullValue m)
-              (combinef ret (reducef (combinef) nil (Get/nullValue m)))
-              ret))))))
+      #(let [ret (combinef)
+             ret (if (Get/root m) (combinef ret (fold (Get/root m) n combinef reducef)) ret)]
+         (if (Get/hasNullValue m)
+           (combinef ret (reducef (combinef) nil (Get/nullValue m)))
+           ret))))
 
   PersistentHashMap$ArrayNode
   (fold [m n combinef reducef]
@@ -50,10 +51,7 @@
             (let [node (aget array idx)]
               (if (not (nil? node))
                 (.add tasks
-                      (reify
-                        Callable
-                        (call [this]
-                          (fold node n combinef reducef)))))))
+                      #(fold node n combinef reducef)))))
       (fold-tasks tasks combinef)))
 
   PersistentHashMap$BitmapIndexedNode
