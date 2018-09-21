7e6b95a8a1e180b5091e06645b015da825d1089d
reborg <reborg@reborg.net>
2018-09-21 16:49:47 +0100

    0.7-wip bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject parallel "0.6"
+(defproject parallel "0.7"
   :description "A library of parallel-enabled Clojure functions"
   :url "https://github.com/reborg/parallel"
   :license {:name "Eclipse Public License"
