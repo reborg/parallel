103f85deaf3e21368ceb318dbbd59c331758ca84
fazekas benedek <fazekasbenedek@fazekass-MBP.home>
2019-03-15 16:24:51 +0000

    Fixes one segment namespaces from `benchmarks` get into jar artifact

--- project.clj
+++ project.clj
@@ -4,8 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  ; :source-paths ["src" "playground" "benchmarks"]
-  :source-paths ["src" "benchmarks"]
   :java-source-paths ["java"]
   :uberjar-name "parallel.jar"
   :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
